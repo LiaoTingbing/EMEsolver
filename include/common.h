d06ab84bee1a214ed2fabed541ab97eeef9077e8
ltb <ltb@DESKTOP-FO8H3TR>
2025-04-29 18:31:58 +0800

    增强 HDF5 数据加载功能和项目配置
    在 CMakeLists.txt 中启用 MSVC 热重载，更新库路径。
    在 common.h 中添加 ModeField 和 Cell 结构体以支持电磁场数据处理。
    在 load_hdf5.h 中声明 loadHdf5Data 函数以加载 HDF5 数据。
    在 main.cpp 中实现 loadHdf5 函数并调用以读取数据。
    在 load_hdf5.cpp 中实现 loadHdf5Data 函数，输出数据集维度信息。

--- include/common.h
+++ include/common.h
@@ -1,9 +1,34 @@
 ﻿#pragma once
  
 
-
+#include<map>
+#include<cstring>
 #include <iostream>
+#define ARMA_USE_HDF5
 #define ARMA_USE_SUPERLU
 #include <armadillo>
 
 using namespace arma;
+using namespace std;
+
+
+struct ModeField
+{
+	//	x方向传播模场
+	cx_mat ex;
+	cx_mat ey;
+	cx_mat ez;
+	cx_mat hx;
+	cx_mat hy;
+	cx_mat hz;
+	cx_double neff;
+	double lambda;
+	vec y;
+	vec z;
+	vec x;
+};
+
+struct Cell
+{
+	field<ModeField> modeFields;
+};
--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 #include "include/common.h"
 
 
+map<string, cube> dev;
+
+void loadHdf5()
+{
+	// 读取x
+
+	string filename = "E:/VisualStudioFiles/EMEsolver/lumerical/out.h5";
+	cube tmp;
+	tmp.load(hdf5_name(filename,"z"));
+	cout << tmp.size();
+	
+
+}
+
 
 int main() {
 	
-
-
+	loadHdf5();
 	return 0;
 }
